2fb339debd6ef22fde0f2a0859ec720fbde07878
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2025-05-01 07:04:40 +0200

    Add core project and model config settings

---
+++ Todo.Core/Settings/AzureOpenAi.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Core.Settings;
+
+public class AzureOpenAi
+{
+    [Required]
+    public required string DeploymentName { get; set; }
+
+    [Required]
+    public required string ApiKey { get; set; }
+
+    [Required]
+    public required string Endpoint { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+}
---
+++ Todo.Core/Settings/Model.cs
@@ -0,0 +1,6 @@
+﻿namespace Todo.Core.Settings;
+
+public class Model
+{
+    public required List<AzureOpenAi> AzureOpenAi { get; set; } = [];
+}
