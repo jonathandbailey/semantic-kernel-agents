31671199e9de195031b3ec7f33cba1d49c19c0dc
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2025-05-23 18:55:24 +0200

    Remove Card Discovery and Add Second Agent

--- Todo.Core/Extensions/ServicesExtensions.cs
+++ Todo.Core/Extensions/ServicesExtensions.cs
@@ -26,7 +26,6 @@ namespace Todo.Core.Extensions
          
             services.AddScoped<IAgentProvider, AgentProvider>();
 
-            services.AddScoped<IAgentDiscovery, AgentDiscovery>();
             services.AddScoped<IAgentStateStore, AgentStateStore>();
 
             services.AddScoped<IAgentFactory, AgentFactory>();
