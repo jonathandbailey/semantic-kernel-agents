02f74813794bf785d94f33ef70e03e8010dcc2f3
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2025-07-19 20:34:16 +0200

    Update Vacation Plan Signal R Handler to invalidate Vacation Plan

--- ui/travel/src/components/Layout/RootLayout.tsx
+++ ui/travel/src/components/Layout/RootLayout.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { Layout, Menu, Splitter } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import vacationPlanService from "../../services/vacationPlanService";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { VacationPlanCatalogItem } from "../../types/vacationPlanCatalogItem";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import VacationPlanPage from "../../pages/VacationPlanPage";
 import ConversationPage from "../../pages/ConversationPage";
 import type { VacationPlanModel } from "../../types/vacationPlan";
@@ -13,9 +14,9 @@ import type { AssistantCommand } from "../../types/assistantCommand";
 
 const { Content, Sider } = Layout;
 
-
-const RootLayout = () => {
-
+const LayoutContent = () => {
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
 
     const { data, refetch } = useQuery({
         queryKey: ["vacationPlanCatalog"],
@@ -44,71 +45,78 @@ const RootLayout = () => {
     };
 
     signalRService.on("command", (response: AssistantCommand) => {
-
         console.log("SignalR Assistent Command:", response);
+
+        if (response.vacationPlanId) {
+            navigate(`/plan/${response.vacationPlanId}`);
+
+            queryClient.invalidateQueries({
+                queryKey: ["vacationPlan", response.vacationPlanId]
+            });
+        }
     });
 
-    return (<>
-        <BrowserRouter>
-            <Layout style={{ height: "100vh" }}>
-                <Sider breakpoint="lg"
-                    style={{ margin: "0px", height: "100vh", overflow: "auto" }}
-                    width={200}
-                    theme="light" >
-                    <Menu
-                        style={{ paddingTop: "32px", paddingLeft: "8px" }}
-                        onClick={handleClick}
+    return (
+        <Layout style={{ height: "100vh" }}>
+            <Sider breakpoint="lg"
+                style={{ margin: "0px", height: "100vh", overflow: "auto" }}
+                width={200}
+                theme="light" >
+                <Menu
+                    style={{ paddingTop: "32px", paddingLeft: "8px" }}
+                    onClick={handleClick}
+                >
+                    <Menu.ItemGroup
+                        key="g3"
+                        title={
+                            <div style={{ color: "gray", paddingLeft: 12, display: "flex", alignItems: "center", justifyContent: "space-between", gap: 8 }}>
+                                <span>Plans</span>
+                                <PlusOutlined style={{ cursor: "pointer" }} onClick={handleAddPlanClick} />
+                            </div>
+                        }
                     >
-                        <Menu.ItemGroup
-                            key="g3"
-                            title={
-                                <div style={{ color: "gray", paddingLeft: 12, display: "flex", alignItems: "center", justifyContent: "space-between", gap: 8 }}>
-                                    <span>Plans</span>
-                                    <PlusOutlined style={{ cursor: "pointer" }} onClick={handleAddPlanClick} />
-                                </div>
-                            }
-                        >
-                            {data?.map((catalogItem: VacationPlanCatalogItem) => (
-                                <Menu.Item key={catalogItem.id}>
-                                    <Link to={`/plan/${catalogItem.id}`}>
-                                        <span style={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap", display: "block", width: "24ch" }}>
-                                            {catalogItem.title}
-                                        </span>
-                                    </Link>
-                                </Menu.Item>
-                            ))}
-                        </Menu.ItemGroup>
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Content style={{
-                        display: "flex",
-                        flexDirection: "column",
-
-                        overflow: "hidden",
-                        backgroundColor: "white",
-                    }}>
-                        <Splitter style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-                            <Splitter.Panel defaultSize="50%" min="20%" max="70%" style={{ padding: 48 }}>
-
-
-                                <Routes>
-                                    <Route path="/plan/:id" element={<VacationPlanPage />} />
-                                </Routes>
+                        {data?.map((catalogItem: VacationPlanCatalogItem) => (
+                            <Menu.Item key={catalogItem.id}>
+                                <Link to={`/plan/${catalogItem.id}`}>
+                                    <span style={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap", display: "block", width: "24ch" }}>
+                                        {catalogItem.title}
+                                    </span>
+                                </Link>
+                            </Menu.Item>
+                        ))}
+                    </Menu.ItemGroup>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Content style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    overflow: "hidden",
+                    backgroundColor: "white",
+                }}>
+                    <Splitter style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+                        <Splitter.Panel defaultSize="50%" min="20%" max="70%" style={{ padding: 48 }}>
+                            <Routes>
+                                <Route path="/plan/:id" element={<VacationPlanPage />} />
+                            </Routes>
+                        </Splitter.Panel>
+                        <Splitter.Panel style={{ padding: 0 }}>
+                            <ConversationPage />
+                        </Splitter.Panel>
+                    </Splitter>
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
 
 
-
-                            </Splitter.Panel>
-                            <Splitter.Panel style={{ padding: 0 }}>
-                                <ConversationPage />
-                            </Splitter.Panel>
-                        </Splitter>
-
-                    </Content>
-                </Layout>
-            </Layout >
-        </BrowserRouter >
-    </>);
+const RootLayout = () => {
+    return (
+        <BrowserRouter>
+            <LayoutContent />
+        </BrowserRouter>
+    );
 }
 
 export default RootLayout;
